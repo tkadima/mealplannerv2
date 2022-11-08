465d60df933d5be5975f924706084e7096d568cb
tkadima <tabithakadima@gmail.com>
2022-11-07 20:11:40 -0500

    Add polish

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Date from '../components/date'
+
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts.js';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -30,12 +34,12 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
